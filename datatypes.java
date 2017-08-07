598a0a47566f08d5afd672277df09d4a9c649efa
nik72619c <30630904+nik72619c@users.noreply.github.com>
2017-08-07 17:31:23 +0530

    Update datatypes.java

--- datatypes.java
+++ datatypes.java
@@ -1,29 +1,59 @@
-class addnos{                    //class begins here
+package practice1;
+                               //PRACTICE FOR DATATYPES
+ strictfp public class addnos {
 
-public static void main(String arr[]){
-
-int a=Integer.parseInt(arr[0]);        //declaring variables
-
-    int b=Integer.parseInt(arr[1]);
-    int sum=0;
-    
-  
-    if(arr.length==0)           // checking fr empty array
-    {System.out.println("sum is 0");
-    
-    }
-    
-    else
-    {
-        
-        for(String w:arr){   //loop fr addition
-        
+	public static void main(String[] args) {
+		
+    System.out.println("hello world");
+    System.out.println("Hello java");
+    byte x=(byte)12;             //explicit type casting as it considers it as int
+    byte y=(byte)1;
+    byte z= (byte)(x+y);
    
-            System.out.println("w is" + w);
-           sum=sum + Integer.parseInt(w);
-        }
-    System.out.println("sum is"+sum);    //printing sum
+    System.out.println(x);
+    System.out.println(z);
+    short a=3456;
+    short b=3425;
+    short c=(short)(a+b);
+    System.out.println("sum is : "+c);
+    float p=10.205068658555959599679f;                    //implicit typecasting
+    System.out.println(p);
+    boolean attendance= true;
+    System.out.println(attendance);    //boolean datatype
+    
+    //Reference datatypes practice
+    
+    String name1="nikhil";
+    String name2=name1;
+    
+    if(name1==name2) {
+    	
+    	System.out.println("same reference");     //ans is same reference
     }
+    
+    else {
+    	
+    	System.out.println("not same reference");
+    }
+    
+   name1="sharma";
+   
+   if(name1==name2) {
+   	
+   	System.out.println("same reference");
+   }
+   
+   else {
+   	
+   	System.out.println("not same reference");  //now different reference
+   	                                          //string objects are immutable
+   }
+   
+   
+   
+	}
+	
+	
+	
+	
 }
-
-}    // end of class
