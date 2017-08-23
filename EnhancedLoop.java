15c8619b47d7427c89ea63ddbd15ec4484d2544a
nik72619c <30630904+nik72619c@users.noreply.github.com>
2017-08-23 22:38:22 +0530

    Add files via upload
    practice for enhanced loop in java

---
+++ EnhancedLoop.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class EnhancedLoop {
+
+	public static void main(String[] args) throws Exception{
+	
+	/*	int x[]= {10,20,30,40,50};
+		ArrayList<Integer> l= new ArrayList<>();
+		
+		l.add(x[0]);
+		l.add(x[1]);
+		l.add(x[2]);
+		l.add(x[3]);
+		
+		l.forEach((e)->System.out.println(e));*/
+	/*	 int b;
+		byte a[]= new byte[100];
+		System.out.println("enter something !: ");
+		for(int i=0;i<100;i++) {
+		 b=System.in.read();
+		System.out.print((char)b);
+		}  */
+		
+		
+		
+		
+	
+
+}
+}
